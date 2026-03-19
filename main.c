2cba74570806e33c45c4b9014e4e732eb2adeb7c
kolon <5819539@stu.kmu.ac.kr>
2026-03-19 18:42:09 +0900

    Implement main function to calculate GCD and LCM

---
+++ main.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"my_math.h"
+
+int main() {
+	char input[100];
+	printf("input two number: ");
+	fgets(input, sizeof(input), stdin);
+
+	int a, b;
+
+	sscanf(input, "%d %d", &a,&b);
+
+	if (a == 0 && b == 0) {
+		printf("error");
+		return 1;
+	}
+
+	my_math gcd = find_Gcd(a, b);
+	my_math Lcm = find_Lcm(a, b);
+	
+	printf("GCD:%d,LCM:%d", gcd, Lcm);
+	
+	return 0;
+}
