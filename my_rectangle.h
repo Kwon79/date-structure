f725ab1c74796c8d24d35db4454395b3aa46a845
kolon <5819539@stu.kmu.ac.kr>
2026-03-20 18:39:09 +0900

    Add my_rectangle header with function declarations

---
+++ my_rectangle.h
@@ -0,0 +1,8 @@
+typedef unsigned int my_rectangle;
+
+extern my_rectangle create(int numerator, int denominator);
+extern my_rectangle perimeter(my_rectangle a, my_rectangle b);
+extern my_rectangle area(my_rectangle a, my_rectangle b);
+extern my_rectangle reduce(my_rectangle f);
+extern int find_gcd(int a, int b);
+extern void print(my_rectangle f);
