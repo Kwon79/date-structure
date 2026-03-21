e7fc00afcfbc04ed51b8fe1e10534285be83a0ba
kolon <5819539@stu.kmu.ac.kr>
2026-03-21 16:42:37 +0900

    Update calcuation.c

--- calcuation.c
+++ calcuation.c
@@ -1,6 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #include"my_rectangle.h"
 
@@ -11,28 +11,8 @@ int main(int c, char** v) {
 		return 1;
 	}
 
-	int n1,d1=0;
-	int n2, d2 = 0;
-
-	sscanf(v[1], "%d/%d", &n1, &d1);
-	sscanf(v[2], "%d/%d", &n2, &d2);
-
-
-	if (d1 == 0 || d2 == 0) {
-		printf("error");
-		return 1;
-	}
-	if (n1 == 0 || n2 == 0) {
-		printf("error");
-		return 1;
-	}
-	if (n1 < 0 || d1 < 0 || n2 < 0 || d2 < 0) {
-		printf("error");
-		return 1;
-	}
-
-	my_rectangle fra1=create(n1,d1);
-	my_rectangle fra2= create(n2, d2);
+	my_rectangle fra1 = parse(v[1]);
+	my_rectangle fra2 = parse(v[2]);
 
 	my_rectangle Area=area(fra1, fra2);
 	my_rectangle Perimeter = perimeter(fra1, fra2);
