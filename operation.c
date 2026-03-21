e000092a2c2c2e6a8d906358ad00149cfb272822
kolon <5819539@stu.kmu.ac.kr>
2026-03-21 15:44:01 +0900

    Refactor fraction calculation input handling

--- operation.c
+++ operation.c
@@ -1,49 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #include"my_fraction.h"
 
 int main() {
-	char input[100];
+    char input[100];
 
-	printf("Please enter the fraction calculation (Available characters:+,-,*,/): ");
-	fgets(input, sizeof(input), stdin);
+    printf("Please enter the fraction calculation (Available characters:+,-,*,/): ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
 
-	int n1, d1, n2, d2;
-	char c;
+    char* token1 = strtok(input, " ");
+    char* token2 = strtok(NULL, " ");
+    char* token3 = strtok(NULL, " ");
 
-	sscanf(input, "%d/%d %c %d/%d", &n1,&d1,&c,&n2,&d2);
+    if (token1 == NULL || token2 == NULL || token3 == NULL) {
+        printf("error");
+        return 1;
+    }
 
-	if (d1 == 0 || d2 == 0) {
-		printf("error");
-		return 1;
-	}
+    my_fraction fra1 = parse(token1);
+    char op = token2[0];
+    my_fraction fra2 = parse(token3);
 
-	my_fraction fra1 = create(n1, d1);
-	my_fraction fra2 = create(n2, d2);
-	my_fraction result = 0;
+    my_fraction result = 0;
 
-	switch (c) {
-	case'+':
-		result = add(fra1, fra2);
-		break;
-	case'-':
-		result = minus(fra1, fra2);
-		break;
-	case'*':
-		result = mult(fra1, fra2);
-		break;
-	case'/':
-		result = divid(fra1, fra2);
-		break;
-	default:
-		printf("error operator!");
-		return 1;
-	}
+    switch (op) {
+    case '+':
+        result = add(fra1, fra2);
+        break;
+    case '-':
+        result = minus(fra1, fra2);
+        break;
+    case '*':
+        result = mult(fra1, fra2);
+        break;
+    case '/':
+        result = divid(fra1, fra2);
+        break;
+    default:
+        printf("error operator!");
+        return 1;
+    }
 
-	my_fraction final = reduce(result); 
-	print(final);
+    my_fraction final = reduce(result);
+    print(final);
 
-	return 0;
+    return 0;
 }
