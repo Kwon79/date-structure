b402802db764a7e3b0e17daf04c55c328133395a
kolon <5819539@stu.kmu.ac.kr>
2026-03-21 16:43:45 +0900

    Add parse function declaration to my_rectangle.h

--- my_rectangle.h
+++ my_rectangle.h
@@ -4,5 +4,6 @@ extern my_rectangle create(int numerator, int denominator);
 extern my_rectangle perimeter(my_rectangle a, my_rectangle b);
 extern my_rectangle area(my_rectangle a, my_rectangle b);
 extern my_rectangle reduce(my_rectangle f);
+extern my_rectangle parse(char* str);
 extern int find_gcd(int a, int b);
 extern void print(my_rectangle f);
