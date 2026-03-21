3a9834f30f1045d5cb0e6d54367f2665c4a7801c
kolon <5819539@stu.kmu.ac.kr>
2026-03-21 16:43:22 +0900

    Enhance my_rectangle.c with parsing and printing functions

--- my_rectangle.c
+++ my_rectangle.c
@@ -1,11 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<math.h>
 
 #include"my_rectangle.h"
 
 my_rectangle create(int numerator, int denominator) {
 	return (numerator << 16) | denominator;
 }
+
+my_rectangle parse(char* str) {
+	int n=0,d = 0;
+
+	if (strchr(str, '/')) {
+		sscanf(str,"%d/%d",&n, &d);
+		if (d == 0) {
+			printf("error!");
+			exit(1);
+		}
+	}
+	else if (strchr(str, '.')) {
+		double val;
+		sscanf(str, "%lf", &val);
+		char* dot = strchr(str, '.');
+		int count = (int)strlen(dot)-1;
+		d = 1;
+		for (int i = 0; i < count;i++) {
+			d *= 10;
+		}
+		n = (int)round(val * d);
+	}
+	else {
+		sscanf(str, "%d", &n);
+		d = 1;
+	}
+
+	if (n <= 0 || d <= 0) {
+		printf("error");
+		exit(1);
+	}
+
+	return create(n, d);
+}
+
 
 my_rectangle perimeter(my_rectangle a, my_rectangle b) {
 	int n1 = a >> 16;
@@ -62,5 +100,7 @@ void print(my_rectangle f) {
 	int n = f >> 16;
 	int d = f & 0xFFFF;
 
-	printf("%d/%d", n, d);
+
+	if (d == 1) { printf("%d", n); }
+	else { printf("%d/%d", n, d); }
 }
